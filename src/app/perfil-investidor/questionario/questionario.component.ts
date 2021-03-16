fc4e7576b75f6c96a6234fa4716b8c5f6b8e2aae
Alexandre <alexandreribeiro@procobservicos.com.br>
2021-03-16 18:54:56 -0300

    Criado função update

--- src/app/perfil-investidor/questionario/questionario.component.ts
+++ src/app/perfil-investidor/questionario/questionario.component.ts
@@ -36,106 +36,104 @@ export class QuestionarioComponent implements OnInit {
     this.loadResource();
   }
 
-  protected setCurrentAction() {
-    if(this.route.snapshot.url[1]?.path){
+  private setCurrentAction() {
+    if (this.route.snapshot.url[1]?.path) {
       this.currentAction = this.route.snapshot.url[1].path
     }
   }
 
   protected loadResource() {
-    if(this.currentAction == "resultado"){
+    if (this.currentAction == "resultado") {
 
       const id: string = this.route.snapshot.params.id;
 
       this.perguntasService.getById(+id)
 
-      .subscribe(resource => {
-        this.respostas = resource.value
-        this.valid = true;
-      },
-      error => {
-        alert('Ocorreu um erro no servidor')
-        this.router.navigateByUrl('perfil-investidor/questionario')
-      }
-      )
+        .subscribe(resource => {
+          this.respostas = resource.value
+          this.valid = true;
+        },
+          error => {
+            alert('Ocorreu um erro no servidor')
+            this.router.navigateByUrl('perfil-investidor/questionario')
+          }
+        )
     }
   }
 
 
-  getAlternativas() {
+  private getAlternativas() {
 
     this.perguntasService.getAlternativas().subscribe(
-      (result : string[]) => {
+      (result: string[]) => {
         this.alternativas = result
       }
     )
   }
 
-  private getPerguntas(){
+  private getPerguntas() {
     this.perguntasService.getPerguntas().subscribe(
-      (result : Perguntas[]) => {
+      (result: Perguntas[]) => {
         this.perguntas = result
       }
     )
   }
 
-  salvar(){
-    if(this.currentAction == "resultado"){
+
+  private create() {
+    this.perguntasService.save(this.respostas).subscribe(
+      (result: any) => {
+        this.router.navigate(['perfil-investidor', 'questionario', 'resultado', result.id])
+      }
+    )
+  }
+
+  private update() {
+    this.perguntasService.update(this.respostas).subscribe(
+      (result: any) => {
+        this.router.navigate(['perfil-investidor', 'questionario', 'resultado', result.id])
+      }
+    )
+  }
+
+  salvar() {
+    if (this.currentAction == "resultado") {
       this.update()
-    }else{
+    } else {
       this.create();
     }
   }
 
-  create(){
-    this.perguntasService.save(this.respostas).subscribe(
-      (result: any) =>{
-        this.router.navigate(['perfil-investidor','questionario', 'resultado', result.id])
-      }
-    )
-  }
-
-  update(){
-    this.perguntasService.update(this.respostas).subscribe(
-      (result: any) =>{
-        this.router.navigate(['perfil-investidor','questionario', 'resultado', result.id])
-      }
-    )
-  }
-
-  home(){
+  home() {
     this.router.navigateByUrl('/');
   }
 
-  validade(){
+  validade() {
 
     let len = 0;
-
     for (const pergunta of this.perguntas) {
-      if(pergunta.type === 'radio'){
-        len ++;
-      }else{
+      if (pergunta.type === 'radio') {
+        len++;
+      } else {
         len += pergunta.alternativas.length;
       }
     }
-
     this.numeroDeAlternativas = len;
-
   }
 
-  isValid(){
+  isValid() {
     this.validade();
 
     let len = 0;
 
     for (const p of this.respostas) {
-      if(p != null){
+      if (p != null) {
         len++;
       }
     }
 
 
-    if(this.numeroDeAlternativas === len + 1){
+    if (this.numeroDeAlternativas === len + 1) {
       this.valid = true;
     }
   }
